1c6d3dd6117c10953cf708a0e7bc9f6bb74a8d34
Kingsley Eneja <eneja.kc@gmail.com>
2022-03-06 17:43:34 +0100

    - Build the scheduler dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+
+import classnames from "classnames";
+import Loading from "./Loading";
+import Panel from "./Panel";
+
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
+class Dashboard extends Component {
+  state = {
+    loading: false,
+    focused: null
+  };
+
+  selectPanel = (id) => {
+    this.setState(prevState => ({
+      focused: prevState.focused !== null ? null : id
+    }));
+  };
+
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
+    const panels = data
+      .filter(
+        panel => this.state.focused === null || this.state.focused === panel.id
+      )
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={this.selectPanel}
+        />
+      ));
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    return <main className={dashboardClasses}>
+      {panels}
+    </main>;
+  }
+}
+
+export default Dashboard;
